614b811900eadf21947b127d67f8c82e052604c3
MoMo <2125395794@qq.com>
2025-05-11 19:31:51 +0800

    chore: bump version to v1.8.20

--- src/main/configs/protocol.js
+++ src/main/configs/protocol.js
@@ -7,5 +7,12 @@ export default {
   'resume-all-task': 'application:resume-all-task',
   'reveal-in-folder': 'application:reveal-in-folder',
   'preferences': 'application:preferences',
-  'about': 'application:about'
+  'about': 'application:about',
+  'download': 'application:new-task',
+  'magnet': 'application:new-bt-task',
+  'torrent': 'application:new-bt-task',
+  'pause': 'application:pause-task',
+  'resume': 'application:resume-task',
+  'remove': 'application:delete-task',
+  'search': 'application:search-task'
 }
--- src/main/core/ProtocolManager.js
+++ src/main/core/ProtocolManager.js
@@ -79,7 +79,7 @@ export default class ProtocolManager extends EventEmitter {
 
   handleMoProtocol (url) {
     const parsed = new URL(url)
-    const { host, search } = parsed
+    const { host, search, pathname } = parsed
     logger.info('[Motrix] protocol parsed:', parsed, host)
 
     const command = protocolMap[host]
@@ -89,6 +89,61 @@ export default class ProtocolManager extends EventEmitter {
 
     const query = search.startsWith('?') ? search.replace('?', '') : search
     const args = parse(query)
+
+    // 处理新增的深度链接命令
+    switch (host) {
+      case 'download':
+        args.type = ADD_TASK_TYPE.URI
+        args.uri = args.url || pathname
+        // 支持更多下载参数
+        if (args.out) args.name = args.out // 支持文件名
+        if (args.dir) args.path = args.dir // 支持下载路径
+        if (args.ua) args.userAgent = args.ua // 支持自定义UA
+        if (args.header) { // 支持自定义请求头
+          try {
+            args.headers = typeof args.header === 'string' 
+              ? JSON.parse(args.header) 
+              : args.header
+          } catch (err) {
+            logger.warn('[Motrix] parse headers error:', err)
+          }
+        }
+        if (args.cookie) args.cookies = args.cookie // 支持Cookie
+        if (args.referer) args.referer = args.referer // 支持Referer
+        if (args.proxy) args.proxy = args.proxy // 支持代理
+        if (args.split) args.split = parseInt(args.split) // 支持分片数
+        break
+      case 'magnet':
+      case 'torrent':
+        args.type = ADD_TASK_TYPE.TORRENT
+        args.uri = args.url || pathname
+        // 支持BT下载参数
+        if (args.out) args.name = args.out
+        if (args.dir) args.path = args.dir
+        if (args.selectfile) { // 选择下载文件
+          try {
+            args.selectedFiles = typeof args.selectfile === 'string'
+              ? args.selectfile.split(',').map(Number)
+              : args.selectfile
+          } catch (err) {
+            logger.warn('[Motrix] parse selected files error:', err)
+          }
+        }
+        break
+      case 'pause':
+      case 'resume':
+      case 'remove':
+        args.gid = args.gid || pathname.replace('/', '')
+        // 删除任务时支持是否删除文件
+        if (host === 'remove' && args.withfile) {
+          args.deleteFile = args.withfile === 'true'
+        }
+        break
+      case 'search':
+        args.keyword = args.keyword || pathname.replace('/', '')
+        break
+    }
+
     global.application.sendCommandToAll(command, args)
   }
 }
